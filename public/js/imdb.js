3709a838dd28f26a246cfc88ad74bd506fa84574
Sedatcan Sonat <sedatcan.sonat@turkcell.com.tr>
2015-03-06 11:25:30 +0200

    asdasdasdasdasdasdasd

--- public/js/imdb.js
+++ public/js/imdb.js
@@ -1,19 +1,35 @@
-/**
- * Created by TTSSONAT on 05/03/2015.
- */
-function ImdbViewModel()
-{
-    self.fromYear=ko.observable(1970);
-    self.toYear=ko.observable(1979);
-    self.genre=ko.observable("Drama");
-    self.genres=ko.observableArray([]);
-    self.movies=ko.observableArray([]);
-    self.search=function()
-    {
-        console.log('Clicked');
-
+function ImdbViewModel(){
+    var self = this;
+    self.fromYear = ko.observable(1970);
+    self.toYear = ko.observable(1979);
+    self.genre = ko.observable("Comedy");
+    self.genres = ko.observableArray([]);
+    self.movies = ko.observableArray([]);
+    self.search = function() {
+        $.ajax({
+            method: 'GET',
+            url: "http://localhost:8001/movies/search/"
+                + self.fromYear() + "/"
+                + self.toYear() + "/"
+                + self.genre(),
+            success: function(movies) {
+                self.movies(movies);
+            }
+        });
     };
-    self.init=function(){
-
+    self.init = function(){
+        $.ajax({
+            method: 'GET',
+            url: "http://localhost:8001/genres",
+            success: function (genres) { //genres=>ajax tan gelen cevap/response. istenen degisken ismi olabilir.
+                self.genres(genres);
+            }
+        });
     };
-}
+};
+
+var viewModel = new ImdbViewModel();
+$('document').ready(function(){
+    ko.applyBindings(viewModel);
+    viewModel.init();
+});
--- server.js
+++ server.js
@@ -1,75 +1,74 @@
-var express =require('express');
+var express = require('express');
 var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/imdb');
 
-
-var directorSchema=new mongoose.Schema({
-    "_id":Number,
-    "name":String,
-    "imdb":String
+var directorSchema = new mongoose.Schema({
+    "_id": mongoose.Schema.Types.ObjectId,
+    "name": String,
+    "imdb": String
 });
 
-var genreSchema=new mongoose.Schema({
-    "_id":mongoose.Schema.Types.ObjectId,
-    "name":String
+var genreSchema = new mongoose.Schema({
+    "_id": mongoose.Schema.Types.ObjectId,
+    "name": String
 });
 
-var movieSchema=new mongoose.Schema({
-    "_id":Number,
-    "title":String,
-    "year":Number,
-    "imdb":String,
-    "directors":[directorSchema],
-    "genres":[genreSchema]
+var movieSchema = new mongoose.Schema({
+    "_id": mongoose.Schema.Types.ObjectId,
+    "title": String,
+    "year": Number,
+    "imdb": String,
+    "directors": [directorSchema],
+    "genres": [genreSchema]
 });
 
-var Movie =mongoose.model('movies2',movieSchema);
-var Movie1 =mongoose.model('movies1',movieSchema);
+var Movie = mongoose.model('movies1', movieSchema);
 
-var app=express();
+var app = express();
 
-app.set('views',_dirName+ '/server/views');
-app.set('view-engine','jade');
-app.use(express.static(_dirName +'/public'));
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/server/views');
+app.use(express.static(__dirname + '/public'));
 
-app.get('/movies/byImdb/:imdb',function(req,res)
-{
-    Movie.find({imdb:req.params.imdb},function(err,result)
-    {
-        res.set('Content-Type','application/json');
+app.get('/movies/byImdb/:imdb', function(req, res){
+    Movie.find({imdb: req.params.imdb}, function (err, result){
+        res.set('Content-Type', 'application/json');
         res.status(200).send(result);
     });
 });
 
-app.get('/genres',function(req,res)
-{
-    Movie1.distinct('genres.name',function(err,result)
-    {
-        res.set('Content-Type','application/json');
+app.get('/movies/byGenre/:genre', function(req, res){
+    Movie.find({'genres.name': req.params.genre}, function (err, result){
+        res.set('Content-Type', 'application/json');
         res.status(200).send(result);
     });
 });
 
-app.get('/movies/search/:from/:to/:genre',function(req,res)
-{
-    Movie1.find({
-        $and :[
-            {"year" : {$gte : req.params.from}},
-            {"year" : {$lte : req.params.to}},
-            {"genres.name" :{$in :[req.params.genre] }}
-        ]}).sort({"year":-1}).exec(function(err,movies){
-        console.log(req.params.to);
-        res.set('Content-Type','application/json');
+app.get('/genres', function(req, res){
+    Movie.distinct('genres.name', function (err, result){
+        res.set('Content-Type', 'application/json');
+        res.status(200).send(result);
+    });
+});
+
+app.get('/movies/search/:from/:to/:genre', function(req, res){
+    Movie.find({
+        $and: [
+            {"year"         : {$gte: req.params.from}},
+            {"year"         : {$lte: req.params.to}},
+            {"genres.name"  : {$in: [req.params.genre]}}
+        ]
+    }).sort({"year": -1}).exec(function (err, movies){
+        res.set('Content-Type', 'application/json');
         res.status(200).send(movies);
     });
-
-
-
 });
-app.get("/",function(req,res)
-{
+
+app.get('/', function(req, res){
     res.render('index');
 });
-var port =8001;
+
+var port = 8001;
 app.listen(port);
+console.log('Server is running...');
